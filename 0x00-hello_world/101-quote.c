04ae0c7f129b42c367eb1717c8a22a36821073ef
midunodunsi <midun99.odunsi@gmail.com>
2023-06-15 18:21:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include <unistd.h>
-/**
- * main -A c program that prints a line to the standard error
- * Return 1 (Success)
- */
-int main(void)
-{
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+#include <sys/syscall.h>
+
+#define STDERR_FILENO 2
+
+int main() {
+    const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    syscall(SYS_write, STDERR_FILENO, message, sizeof(message) - 1);
+    return 1;
 }
