d9d826b18297407fbeb0abc5a51d32e179dfa3b1
midunodunsi <midun99.odunsi@gmail.com>
2023-06-17 08:39:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,14 @@
-#include <unistd.h>
-#include <sys/syscall.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define STDERR_FILENO 2
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
 
-int main() {
-    const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    syscall(SYS_write, STDERR_FILENO, message, sizeof(message) - 1);
-    return 1;
+int main(void) 
+{
+	write(STDOUT_FLIENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
 }
