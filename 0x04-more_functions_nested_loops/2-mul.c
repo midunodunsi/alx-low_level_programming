202156557ecc3460cab8221a03dffae05a8b1f3c
midunodunsi <midun99.odunsi@gmail.com>
2023-06-23 21:39:03 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiply 2 init
+ * @a: first init 
+ * @v: second init
+ * Return: addition of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return(a*b);
+}
