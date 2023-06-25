5c05ad20debcdd1ea54b56f4790c0dad701bd07b
midunodunsi <midun99.odunsi@gmail.com>
2023-06-24 22:58:07 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - Prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
