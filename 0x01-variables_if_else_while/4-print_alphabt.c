314bc9b58b1be77e9602bb5640b8534b0becbadb
midunodunsi <midun99.odunsi@gmail.com>
2023-06-24 23:36:34 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,15 +9,14 @@
  */
 int main(void)
 {
-	int i;
-	
-	 for (i = 97; i < 123; i++)
-	 {
-		 if (i != 101 && i != 113)
-		 {
-			 putchar(i);
-		 }
-	 }
-	 putchar('\n');
-	 return (0);
+	int i;	
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
