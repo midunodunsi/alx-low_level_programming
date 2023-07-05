693f36233979c8104309e2885f08a83bb1a48795
midunodunsi <midun99.odunsi@gmail.com>
2023-07-05 06:58:03 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+int _sqrt(int n, int i);
+
+/**
+ * _sqrt_recursion - Entry point
+ * @n: input
+ *
+ * Return: the natural square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - Entry point
+ * @n: input
+ * @i: input
+ *
+ * Return: the natural square root
+ */
+
+int _sqrt(int n, int i)
+{
+	int sqrt = i *i;
+
+	if (sqrt > n)
+		return (-1);
+
+	if (sqrt == n)
+		return (i);
+
+	return (_sqrt(n, i + 1));
+}
