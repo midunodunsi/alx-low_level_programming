83fc82cbe7a76c983d0b6406078fcbf7c673545b
midunodunsi <midun99.odunsi@gmail.com>
2023-07-07 11:24:06 -0700

    files

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: the cahracter to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+        return (c >= 'a' && c <= 'Z');
+}
