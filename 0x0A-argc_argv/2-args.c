6525db0766f1bab1354e92750a3aaa3e60a46735
midunodunsi <midun99.odunsi@gmail.com>
2023-07-07 21:24:50 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all the args content
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
