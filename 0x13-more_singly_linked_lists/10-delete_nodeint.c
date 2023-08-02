99d27d4906dafe0061af6fd1b9cbfe32c5efe983
midun <midun99.odunsi@gmail.com>
2023-08-01 22:50:56 -0700

    group push

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index of a linked list.
+ * @head: pointer to the dirst element in the list
+ * @index: index of the code to delete
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *tmp, *next;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+		return (1);
+	}
+	tmp = *head;
+	for (i = 0; i < index - 1; i++)
+	{
+		if (tmp->next == NULL)
+			return (-1);
+		tmp = tmp->next;
+	}
+	next = tmp->next;
+	tmp->next = next->next;
+	free(next);
+	return (1);
+}
---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _r - reallocation memory for an array of pointers
+ * @list: the old list
+ * @size: size of the new list
+ * @new: new node to add to the list
+ *
+ * Return: pointer to the new linr
+ */
+
+
