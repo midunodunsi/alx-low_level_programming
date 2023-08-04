8a5623b88a43e6fc7ac5cff15de627103781c036
Inumidun Odunsi <midun99.odunsi@gmail.com>
2023-08-04 03:38:15 +0100

    Delete 0x0E-structures_typedef directory

--- 0x0E-structures_typedef/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif
